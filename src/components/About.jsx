f80d07cd5d67162d8f5cf27afbd634484a73f739
Eemayas <prashantmanandhar2002@gmail.com>
2023-05-13 18:01:57 +0545

    retry

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,63 +1,9 @@
-import React from "react";
-import { Tilt } from "react-tilt";
-import { motion, spring } from "framer-motion";
-import { styles } from "../style";
-import { services } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion.js";
-import { SectionWrapper } from "../hoc";
-const ServiceCard = ({ index, title, icon }) => {
-  return (
-    <Tilt className="xs:w-[250px] w-full ">
-      <motion.div
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card "
-        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-      >
-        <div
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center "
-          options={{ max: 45, scale: 1, speed: 450 }}
-        >
-          <img
-            src={icon}
-            alt={title}
-            className="w-16 h-16 object-contain "
-          ></img>
-          <h3 className="text-white text-[20px] font-bold text-center ">
-            {title}
-          </h3>
-        </div>
-      </motion.div>
-    </Tilt>
-  );
-};
-const About = () => {
-  const Bio = `"Hello there! I am a highly skilled computer engineering professional with expertise in various programming languages such as C, C++, Flutter, Dart, React, HTML, CSS, and JavaScript. I am a quick learner and always eager to take on new challenges that help me further expand my skillset. In my free time, I love to indulge in my hobbies of cycling and reading novels and comics. My passion for technology and innovation drives me to continuously learn and stay up-to-date with the latest trends in the industry. With my strong work ethic and exceptional problem-solving skills, I am confident in my ability to contribute to any project I am a part of. Thank you for taking the time to read my bio, and I look forward to potentially working with you!"`;
-  return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
-      </motion.div>
-      <motion.p
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        variants={fadeIn("", "", 0.1, 1)}
-      >
-        {Bio}
-      </motion.p>
-      <div className="mt-20 flex flex-wrap gap-10">
-        {/* {console.log(services.length())} */}
-        {services.map((service, index) => {
-          return (
-            <ServiceCard
-              key={service.title}
-              index={index}
-              title={service.title}
-              {...service}
-            />
-          );
-        })}
-      </div>
-    </>
-  );
-};
+import React from 'react'
 
-export default SectionWrapper(About, "about");
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,67 +1,9 @@
-import React from "react";
+import React from 'react'
 
-import { BallCanvas } from "./canvas";
-import { SectionWrapper } from "../hoc";
-import { technologies } from "../constants";
-import { motion } from "framer-motion";
-import { Tilt } from "react-tilt";
-import { fadeIn, textVariant } from "../utils/motion.js";
-const TechCard = ({ index, title, icon }) => {
-  return (
-    <Tilt className="xs:w-[120px] w-[120px] ">
-      <motion.div
-        className="w-full green-pink-gradient p-[1px] rounded-[30px] shadow-card "
-        variants={fadeIn("right", "spring", 0.25 * index, 0.55)}
-      >
-        <div className="bg-tertiary rounded-[30px] py-5 px-5"
-          // className="bg-tertiary rounded-[20px] px-12 min-h-[120px] flex flex-col justify-evenly items-center "
-          // options={{ max: 45, scale: 1, speed: 450 }}
-        >
-          <img
-            src={icon}
-            alt={title}
-            className="w-full h-full object-contain "
-          ></img>
-          {/* <h3 className="text-white text-[20px] font-bold text-center ">
-            ddd
-          </h3> */}
-        </div>
-      </motion.div>
-    </Tilt>
-  );
-};
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology,index) => (<TechCard index={index} key={technology.name} icon={technology.icon} />
-         
-      ))}
-    </div>
-  );
-}; 
-{/* <div
-            className="w-28 h-28 rounded-[40px] bg-tertiary"
-            key={technology.name}
-          > */}
-            {/* <img
-              className="h-[100%] w-[100%] "
-              src={technology.icon}
-              alt={technology.name}
-            /> */}
-           
-  
-            {/* <BallCanvas icon={technology.icon} /> */}
-          {/* </div> */}
-// const Tech = () => {
-//   return (
-//     <div className='flex flex-row flex-wrap justify-center gap-10'>
-//       {technologies.map((technology) => (
-//         <div className='w-28 h-28' key={technology.name}>
-//           <BallCanvas icon={technology.icon} />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
+    <div>Tech</div>
+  )
+}
 
-export default SectionWrapper(Tech, "tech");
+export default Tech
