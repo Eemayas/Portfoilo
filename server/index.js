87f29c568ee2230b54518603660d8dc33db6ec62
Ëemayas <prashantmanandhar2002@gmail.com>
2023-09-17 00:03:30 +0545

    hidden

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import router from "./routes/routes.js"
+import dotenv from "dotenv"; 
+
+
+dotenv.config();
+
+
 const app = express();
 
 app.use(cors());
@@ -10,15 +16,13 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/portfoilo",router );
 
-const CONNECTION_URL =
-  "mongodb+srv://Eemayas:Eemayas123@cluster0.urn9i9g.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL =process.env.CONNECTION_URL;
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT||5000 ;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.error(error.message));
-
